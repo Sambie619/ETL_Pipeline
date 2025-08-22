5405ea8ee5f9f5c4c8ed611aa4b7e276a62459a2
Sambie619 <sabinshah619@gmail.com>
2025-08-22 23:58:12 +0530

    Added codes in weather_etl.py

--- dags/weather_etl.py
+++ dags/weather_etl.py
@@ -1,3 +1,22 @@
 from airflow import DAG
 from airflow.providers.https.hooks.http import HttpHook
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.decorators import task
+from airflow.utils.dates import days_ago
+
+LATITUDE='51.5074'
+LONGITUDE='-0.1278'
+POSTGRES_CONN_ID='postgres_default'
+API_CONN_ID='open_meteo_api'
+default_args={
+    'owner':'airflow',
+    'start_date':days_ago(1)
+}
+
+with DAG(dag_id='weather_etl_pipeline',default_args=default_args,schedule_interval='@daily',
+         catchup=False)as dags:
+    @task()
+    def extract_weather_data():
+        
+        
+        http_hook=HttpHook(http_conn_id=API_CONN_ID,method='GET')
